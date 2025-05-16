ed39d9cf15fdd610719ce8ec3069521d20c512af
Manu <mandrescabral@gmail.com>
2025-05-16 04:35:56 -0300

    d

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import { redirect } from "next/navigation";
 export default async function Home() {
   const sesion = await obtenerSesion();
   if (sesion) redirect("/panel");
-
   return (
     <main className="flex min-h-screen items-center justify-center bg-white p-4">
       <LoginPage />
